d80ba9a4b2b863f155e81aae26cc5a0f931f539d
Ririsoft <riri@ririsoft.com>
2021-07-06 11:32:41 +0200

    impl debug/01-parse

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,10 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _input = parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    //eprintln!("TOKEN: {}", expanded);
+    TokenStream::new().into()
 }
